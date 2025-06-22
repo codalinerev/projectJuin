ae0e41753a8e45aeed8c9fac0789d03cd51b7345
codalinerev <davali2004@yahoo.com>
2025-06-23 00:55:00 +0200

    apple images on white

--- Scenes/MenuScene.cs
+++ Scenes/MenuScene.cs
@@ -5,14 +5,17 @@ namespace projectJuin;
 
 public class MenuScene : Scene
 {
+     Texture2D appleNormal = Raylib.LoadTexture("assets/apple_basic.png)");
     public override void Load()
     {
         Console.WriteLine("Load scene menu");
+       
     }
     public override void Draw()
     {
         Raylib.DrawText("Snake", 300, 300, 60, Color.Green);
         Raylib.DrawText("Press enter to start", 500, 300, 20, Color.Black);
+        Raylib.DrawTexture(appleNormal, 600, 300, Color.Red);
     }
     
     public override void Unload()
