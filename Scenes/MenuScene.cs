2d51138c3030f2b5fdc9e3a8cd3a740f4faf7955
codalinerev <davali2004@yahoo.com>
2025-06-23 01:05:39 +0200

    fix: correct apple texture loading and adjust drawing positions

--- Scenes/MenuScene.cs
+++ Scenes/MenuScene.cs
@@ -5,17 +5,19 @@ namespace projectJuin;
 
 public class MenuScene : Scene
 {
-     Texture2D appleNormal = Raylib.LoadTexture("assets/apple_basic.png)");
+    Texture2D appleNormal = Raylib.LoadTexture("assets/apple_basic.png");
+    Texture2D appleCrazy = Raylib.LoadTexture("assets/apple_fuzzy.png");
     public override void Load()
     {
         Console.WriteLine("Load scene menu");
-       
+
     }
     public override void Draw()
     {
         Raylib.DrawText("Snake", 300, 300, 60, Color.Green);
         Raylib.DrawText("Press enter to start", 500, 300, 20, Color.Black);
-        Raylib.DrawTexture(appleNormal, 600, 300, Color.Red);
+        Raylib.DrawTexture(appleNormal, 680, 400, Color.White);
+        Raylib.DrawTexture(appleCrazy, 600, 400, Color.White);
     }
     
     public override void Unload()
