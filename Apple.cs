14eb1c2f904d5269ff4800e995c366b40667c042
codalinerev <davali2004@yahoo.com>
2025-06-23 00:38:06 +0200

    apple images

--- Apple.cs
+++ Apple.cs
@@ -1,5 +1,6 @@
 namespace projectJuin;
 using System;
+using System.Formats.Asn1;
 using System.Numerics;
 using Raylib_cs;
 
@@ -9,6 +10,9 @@ public class Apple
     public Coordinates coordinates { get; private set; }
     private Grid grid;
     private bool surprise = false;
+    Texture2D appleNormal = Raylib.LoadTexture("assets/apple_basic.png");
+    Texture2D appleCrazy = Raylib.LoadTexture("assets/apple_fuzzy.png");
+    Texture2D texApple;
 
     public string typeApple = "Normal";
     public string lastApple = "Normal";
@@ -25,7 +29,7 @@ public class Apple
     {
         Random randomT = new Random();
         int i = randomT.Next(0,typesApple.Length);
-        return typesApple[i];       
+        return typesApple[i];  // for now, only two types of apples, Normal and Crazy     
     } 
 
     public void Respawn()
@@ -37,8 +41,13 @@ public class Apple
     public void Draw()
     {
         var pos = grid.GridToWorld(coordinates);
-        pos += new Vector2(grid.cellSize * 0.5f, grid.cellSize * 0.5f);
-        Raylib.DrawCircle((int)pos.X, (int)pos.Y, grid.cellSize * 0.5f, Color.Red);
+        //for debug  Raylib.DrawTexture(appleNormal, 300, 300, Color.White);
+        //for debug Raylib.DrawTexture(appleCrazy, 400, 300, Color.White);
+        //pos += new Vector2(grid.cellSize * 0.5f, grid.cellSize * 0.5f);
+        //Raylib.DrawCircle((int)pos.X, (int)pos.Y, grid.cellSize * 0.5f, Color.Red);
+        texApple = appleNormal;
+        if (typeApple == "Crazy") texApple = appleCrazy;
+        Raylib.DrawTexture(texApple, (int)pos.X, (int)pos.Y, Color.White);
         Raylib.DrawText($"Last Apple was {lastApple}", 1010, 500, 15, Color.Black);
         if (lastApple == "Crazy")
         {            
