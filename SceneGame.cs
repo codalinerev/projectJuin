f696c828fc7cd1ae7d44dd48c0a0fd32dd339304
codalinerev <davali2004@yahoo.com>
2025-06-25 08:39:37 +0200

    cleaner

--- SceneGame.cs
+++ SceneGame.cs
@@ -10,15 +10,13 @@ public class SceneGame : Scene
     Snake snake;
     Apple apple;
 
-
-
     ScoreGui score = new ScoreGui();
     static IGameController gameController = Services.Get<IGameController>();
     int apples = gameController.GetApplesEaten();
     int length = gameController.GetSnakeLength();
-    int shoots = gameController.GetShoots();
+    //int shoots = gameController.GetShoots(); // idée pas encore implementée
     int pauses = gameController.GetPauses();
-    public string lastApple = "Normal";
+
 
     Timer timer;
     Timer gameOverTimer;
@@ -50,24 +48,24 @@ public class SceneGame : Scene
 
     public override void Draw()
     {
-        //base.Draw();
         grid.Draw();
-        //grid.DrawMini();
         snake.Draw();
-        apple.Draw();
-        score.Draw();
-        //Raylib.DrawRectangleLines(1010, 200, 100, 50, Color.Black);
+        apple.Draw();   
         Raylib.DrawText("press SPACE for pause", 1000, 200, 15, Color.Blue);
         Raylib.DrawText("move snake with arrows", 1000, 100, 15, Color.DarkGreen);
-        //Raylib.DrawText("apple is at " + apple.coordinates, 1010, 200, 20, Color.Red);
-        //Raylib.DrawText("snake is at " + snake.Head, 1010, 240, 20, Color.Blue);
-        //Raylib.DrawText($"Apples Eaten : {apples}", 1010, 300, 15, Color.Black);
-        //Raylib.DrawText($"Shoots : {shoots} :", 1010, 350, 15, Color.Black);
         Raylib.DrawText($"Pauses : {pauses}", 1010, 400, 15, Color.Black);
         Raylib.DrawText($"Apples Eaten : {apples}", 1010, 300, 15, Color.Black);
+        if (snake.isFuzzy)
+        {
+            Raylib.DrawText("!!!!!", 1030, 500, 40, Color.Red);
+            Raylib.DrawText("FUZZY", 1010, 540, 30, Color.Red);
+            Raylib.DrawText("SNAKE", 1020, 570, 30, Color.Red);
+            Raylib.DrawText("apple was CRAZY", 1010, 600, 15, Color.Red);
+            Raylib.DrawText("invert commands!!!", 1010, 630, 15, Color.Red);
+        }
 
         if (IsGameOver)
-            Raylib.DrawText("GAMEOVER", 100, 100, 20, Color.Red);
+                Raylib.DrawText("GAMEOVER", 100, 100, 20, Color.Red);
     }
 
     public override void Unload()
@@ -92,8 +90,7 @@ public class SceneGame : Scene
         if (snake.IsCollidingWithApple(apple))
         {
             Console.WriteLine("Snake collided with apple at: " + apple.coordinates);
-            lastApple = apple.typeApple;
-            if (apple.typeApple == "Crazy")
+            if (apple.typeApple == Apple.appleType.Crazy)
                 snake.isFuzzy = true;
             else snake.isFuzzy = false;
                  
